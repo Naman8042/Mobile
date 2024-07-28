431820a763bfdb0d733b7b8e388f07a2df109a03
Naman8042 <131790382+Naman8042@users.noreply.github.com>
2024-07-28 14:45:21 +0530

    14:45

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Mountain from '../assets/Mountain.jpg'
+import Mountain from '../assets/background.png'
 import {Link} from 'react-router-dom'
 
 const Home = () => {
@@ -7,10 +7,10 @@ const Home = () => {
     <div className="w-screen h-screen overflow-hidden relative">
       <img src={Mountain} alt="Mountain" className="w-full h-full object-cover" />
       <p className='absolute top-12 text-center w-full text-6xl text-white font-eater'>Aspen</p>
-      <div className='bottom-28 text-white absolute px-6'>
-        <p className='text-2xl'>Plan your</p>
-        <p className='text-5xl font-normal'>Luxurious</p>
-        <p className='text-5xl font-normal'>Vacation</p>
+      <div className='bottom-28 text-whit absolute px-6'>
+        <p className='text-2xl font-semibold'>Plan your</p>
+        <p className='text-5xl font-bold'>Luxurious</p>
+        <p className='text-5xl font-bold'>Vacation</p>
       </div>
       <div className='absolute bottom-10 flex items-center justify-center w-full px-6'>
       <button className='w-full bg-blue-700 py-3 rounded-2xl text-white'>
