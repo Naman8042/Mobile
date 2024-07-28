7db7996a5650136b550bb92bd7ff3a5f64ff86b0
Naman8042 <131790382+Naman8042@users.noreply.github.com>
2024-07-28 14:40:11 +0530

    14:40

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Mountain from '../assets/Mountain2.jpg'
+import Mountain from '../assets/Mountain.jpg'
 import {Link} from 'react-router-dom'
 
 const Home = () => {
